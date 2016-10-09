af2b30e63b77f4fc562936b0b204ec0e6338ee84
ronne <ronne@HIRUKE-NB>
2016-10-09 16:56:41 -0300

    .

--- DesfacaFacil/DesfacaFacil/App_Start/RouteConfig.cs
+++ DesfacaFacil/DesfacaFacil/App_Start/RouteConfig.cs
@@ -12,7 +12,6 @@ namespace DesfacaFacil
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
