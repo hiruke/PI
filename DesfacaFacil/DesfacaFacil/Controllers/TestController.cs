a7870251b9b20176f494934d78eece8e8a4478ed
ronne <ronne@HIRUKE-NB>
2016-11-06 15:33:14 -0200

    Criada view de testes

---
+++ DesfacaFacil/DesfacaFacil/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DesfacaFacil.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Teste()
+        {
+            return View();
+        }
+    }
+}
