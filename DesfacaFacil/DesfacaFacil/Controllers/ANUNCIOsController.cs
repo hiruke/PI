fc92c0db916ffbd4d344974417ba46a6f1c26bee
ronne <ronne@HIRUKE-NB>
2016-11-15 14:50:50 -0200

    a

--- DesfacaFacil/DesfacaFacil/Controllers/ANUNCIOsController.cs
+++ DesfacaFacil/DesfacaFacil/Controllers/ANUNCIOsController.cs
@@ -7,22 +7,33 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DesfacaFacil.Models;
+using System.Diagnostics;
 using DAL;
+using System.Runtime.InteropServices;
 
 namespace DesfacaFacil.Controllers
 {
     public class ANUNCIOsController : Controller
     {
-        private Entidades db = new Entidades();
-
-        // GET: ANUNCIOs
-        public ActionResult Index()
+        IDBController dbcontroller = new DBController();
+        public ActionResult Criar()
         {
-            IDBController dbcontroller = new DBController();
-            List<DBAnuncios> anuncios = dbcontroller.getAnuncios();
-            return View(anuncios.ToList());
+            List<DBCategorias> categorias = dbcontroller.getCategorias();
+            ViewBag.categorias = categorias;
+            Debug.WriteLine(dbcontroller.getCategorias()[0].nome);
+            return View();
         }
 
+        [HttpGet]
+        public ActionResult Criar(string titulo, string descricao, int duracao, int categoria, string tipo)
+        {
+            Debug.WriteLine(tipo);
+            Debug.WriteLine(titulo);
+            Debug.WriteLine(descricao);
+            Debug.WriteLine(duracao);
+            Debug.WriteLine(categoria);
+            return View("Criar");
+        }
 
         [HttpGet]
         public ActionResult Visualizar(int id)
